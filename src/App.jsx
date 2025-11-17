f001563754436b0f3f4112526afb26e14a976f51
mankal-27 <mkalkutagi27@gmail.com>
2025-11-17 22:30:57 +0530

    Switch to HashRouter for Github pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate, HashRouter } from "react-router-dom";
 import UploadPage from "./pages/UploadPage.jsx";
 import DashboardPage from "./pages/DashboardPage.jsx";
 //import ReportPage from "./pages/ReportPage.jsx";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to = "/upload" replace />}/>
-        <Route path="/upload" element={<UploadPage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
-      </Routes>
-    </BrowserRouter>
+   <HashRouter>
+  <Routes>
+    <Route path="/" element={<UploadPage />} />
+    <Route path="/dashboard" element={<DashboardPage />} />
+  </Routes>
+</HashRouter>
   )
 }
